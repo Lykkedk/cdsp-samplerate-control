4076d9c24fc5e00178385b37c4b18f8b9c14c035
Jesper Lykke <62839593+Lykkedk@users.noreply.github.com>
2020-11-18 09:20:02 +0100

    Update StartServer.sh

--- StartServer.sh
+++ StartServer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Script to start the CamillaDSP server  192.168.1.XX:5000 (with correct IP address)
 
-su tc -c  "/usr/local/bin/python3 /mnt/mmcblk0p2/tce/cdsp_webinterface/Backend-0.5.2/main.py" &
+su tc -c  "/usr/local/bin/python3 /mnt/mmcblk0p2/tce/cdsp_webinterface/Backend_0.5.2/main.py" &
 
 exit
